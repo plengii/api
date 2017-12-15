bd69491cf6dcdbe9f947ca3a876eefb406e5d300
nattakan <nattakan.t@dld.go.th>
2017-12-15 09:57:51 +0700

    สร้างไฟล์เชื่อม db mysql

--- index.php
+++ index.php
@@ -3,6 +3,9 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 require 'vendor/autoload.php';
+require 'setting.php';
+
+$app = new \Slim\App(["settings" => $config]);
 
 $app = new \Slim\App;
 require 'routes/hello.php';
